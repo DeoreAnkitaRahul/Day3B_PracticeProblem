030702721ab74e75534e9d08845616f55614e541
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-05-06 15:54:02 +0530

    [Ankita] Add : UC2-Example Of Interface

---
+++ Day3B_PracticeProblem/Day3B_PracticeProblem/InterfaceDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3B_PracticeProblem
+{
+    public interface InterfaceDemo
+    {
+       void animalSound(); 
+        }
+        class Pig : InterfaceDemo
+        {
+            public void animalSound()
+            {
+                
+                Console.WriteLine("The pig says: wee wee");
+            }
+        }
+
+     
+}
--- Day3B_PracticeProblem/Day3B_PracticeProblem/Program.cs
+++ Day3B_PracticeProblem/Day3B_PracticeProblem/Program.cs
@@ -4,9 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            ConstructorTypes constructor = new ConstructorTypes();
-            ConstructorTypes types = new ConstructorTypes(345, "Ankita");
-            ConstructorTypes copy = new ConstructorTypes(types);
+            //ConstructorTypes constructor = new ConstructorTypes();
+            //ConstructorTypes types = new ConstructorTypes(345, "Ankita");
+            //ConstructorTypes copy = new ConstructorTypes(types);
+
+            //Interface
+            Pig pig = new Pig();
+            pig.animalSound();
+            Console.ReadKey();
         }
     }
 }
